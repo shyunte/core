fdac026ad8f5a064e56f7e620d0257506820dfd9
hyun12 <sht4441@gmail.com>
2023-02-13 18:26:08 +0900

    create Order

---
+++ src/main/java/Spring/core/MemberApp.java
@@ -0,0 +1,18 @@
+package Spring.core;
+
+import Spring.core.member.Grade;
+import Spring.core.member.Member;
+import Spring.core.member.MemberService;
+import Spring.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
